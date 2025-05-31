9ce20914ae788d61bbadfaed6ab6761d9f8b74a7
DrCBeatz <brynecarruthers@gmail.com>
2025-05-30 20:56:11 -0400

    move homepage inline css & js to home.css & home.js

---
+++ static/js/home.js
@@ -0,0 +1,19 @@
+// static/js/home.js
+
+// Initialize Swiper JS
+
+const swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      effect: "fade",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 4000,
+      }
+});
